8aa1b8cd846465b2115f3b6751592b21c4c33e7f
hugyan <hugyanyi@gmail.com>
2017-07-06 00:49:27 +0800

    heros 图片处理

---
+++ hero /pages/legends/legends.js
@@ -0,0 +1,84 @@
+// pages/legends/legends.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  record:[
+    {
+    img: "../img/7.png",
+    time: '2017-06-10 12:48:50',
+    kda:'1179991680 2/11/2 负'
+    },{
+    img: "../img/8.png",
+    time: '2017-06-10 12:13:47',
+    kda:'1179966154 6/10/3 负'
+    },{
+    img: "../img/9.png",
+    time:'2017-06-10 12:48:50',
+    kda:'1179973823 2/5/7 负'
+    },{
+    img: "../img/10.png",
+    time: '2017-06-10 12:48:50',
+    kda:'1179973823 3/8/4 负'
+    }
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
